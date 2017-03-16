135a4f47b42889763d830e378c46555313ae720d
cong <cc.der.com>
2017-03-16 16:06:35 +0800

    动态更改时间

--- ion-scanner-v2/src/pages/scan/scan.ts
+++ ion-scanner-v2/src/pages/scan/scan.ts
@@ -10,9 +10,9 @@ import { HomePage } from '../home/home';
 })
 export class ScanPage {
 
-date: string = new Date().toISOString();
-
-
+date: string = new Date().toLocaleDateString();
+time: string = new Date().toLocaleTimeString();
+week: string = new Date().toDateString();
   
 
   constructor(
@@ -22,7 +22,27 @@ date: string = new Date().toISOString();
   ) { 
   
   }
-
+ionViewDidLoad() {
+   this.week  = this.week.substr(0,3);
+   this.time = this.time.substr(6,5);
+        switch(this.week){
+          case 'Mon':  this.week = '星期一';
+           break;    
+          case 'Tue':  this.week = '星期二';
+            break;
+          case 'Wed':  this.week = '星期三';
+            break;
+          case 'Thu':  this.week = '星期四';
+            break;
+          case 'Fir':  this.week = '星期五';
+            break;
+          case 'Sat':  this.week = '星期六';
+            break;
+          case 'Sun':  this.week = '星期日';
+            break;
+          default :break;
+    }
+  }
   openMenu() {
     let actionSheet = this.actionsheetCtrl.create({
      
